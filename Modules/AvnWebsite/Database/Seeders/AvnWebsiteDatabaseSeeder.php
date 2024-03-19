2b4940999129826a133abe2b2d64fba7b6f79f46
blackbloww <157106503+blackbloww@users.noreply.github.com>
2024-03-19 10:19:41 +0700

    Initial commit

---
+++ Modules/AvnWebsite/Database/Seeders/AvnWebsiteDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\AvnWebsite\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AvnWebsiteDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
