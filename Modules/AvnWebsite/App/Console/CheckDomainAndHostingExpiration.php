cc2f1a6bc91da2af579f1f47bafbe927a5f36317
Trường <truonglcpl16@gmail.com>
2024-04-08 08:32:28 +0700

    update

--- Modules/AvnWebsite/App/Console/CheckDomainAndHostingExpiration.php
+++ Modules/AvnWebsite/App/Console/CheckDomainAndHostingExpiration.php
@@ -4,56 +4,70 @@ namespace Modules\AvnWebsite\App\Console;
 
 use Illuminate\Console\Command;
 use Modules\AvnWebsite\App\Models\AvnWebsites;
+use Utilities\Telegram;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 
 class CheckDomainAndHostingExpiration extends Command
 {
     protected $signature = 'website:check-expiration';
-    public static function handle()
+
+    public function handle()
     {
         $websites = AvnWebsites::with('user')->get();
 
         foreach ($websites as $website) {
-            if (self::isExpiringSoon($website->domain_date_expried) || self::isExpiringSoon($website->hosting_date_expried)) {
-                self::sendNotification($website);
+            if ($this->isExpiringSoon($website->domain_date_expried) || $this->isExpiringSoon($website->hosting_date_expried)) {
+                $this->sendNotification($website);
             }
         }
     }
 
-    public static function isExpiringSoon($expirationDate)
+    public function isExpiringSoon($expirationDate)
     {
         $expirationTimestamp = strtotime($expirationDate);
         $currentTimestamp = strtotime('now');
-        $secondsInThreeDays = 3 * 24 * 60 * 60;
+        $secondsInThreeDays = 5 * 24 * 60 * 60;
 
         return ($expirationTimestamp - $currentTimestamp) <= $secondsInThreeDays;
     }
 
-    public static function sendNotification($website)
+    public function sendNotification($website)
     {
         $user = $website->user;
-        if ($user && $user->email) {
-            try {
+        if ($user) {
 
-                $emailBody = "Website: $website->url\n";
-                if ($website->domain_date_expried !== null && self::isExpiringSoon($website->domain_date_expried)) {
-                    $emailBody .= "Domain expiration date: $website->domain_date_expried (Domain is expiring soon)\n";
+            $notificationMessage = "Website: $website->url\n";
+
+            if ($website->domain_date_expried !== null && self::isExpiringSoon($website->domain_date_expried)) {
+                $notificationMessage .= "Domain expiration date: $website->domain_date_expried (Domain will expire in 5 days)\n";
+            }
+
+            if ($website->hosting_date_expried !== null && self::isExpiringSoon($website->hosting_date_expried)) {
+                $notificationMessage .= "Hosting expiration date: $website->hosting_date_expried (Hosting will expire in 5 days)\n";
+            }
+
+            if ($user->email) {
+                try {
+                    Mail::raw($notificationMessage, function ($message) use ($user) {
+                        $message->to($user->email)->subject('Website Expiration Notification');
+                    });
+                } catch (\Exception $e) {
+                    \Log::error('Failed to send email notification: ' . $e->getMessage());
                 }
-
-                if ($website->hosting_date_expried !== null && self::isExpiringSoon($website->hosting_date_expried)) {
-                    $emailBody .= "Hosting expiration date: $website->hosting_date_expried (Hosting is expiring soon)\n";
+            }
+            if ($user->tele_id) {
+                try {
+                    Telegram::sendMessage($user, $notificationMessage);
+                } catch (\Exception $e) {
+                    \Log::error('Failed to send Telegram notification: ' . $e->getMessage());
                 }
-
-
-                Mail::raw($emailBody, function ($message) use ($user) {
-                    $message->to($user->email)->subject('Website Expiration Notification');
-                });
-            } catch (\Exception $e) {
-                \Log::error('Failed to send notification: ' . $e->getMessage());
+            }
+            if (!$user->email && !$user->tele_id) {
+                \Log::warning('User has no email or Telegram chat ID: ' . $user->id);
             }
         } else {
-            \Log::warning('User has no email or user not found for website: ' . $website->url);
+            \Log::warning('User not found for website: ' . $website->url);
         }
     }
 }
